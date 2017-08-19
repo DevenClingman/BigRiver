1ed6d2a14797b575e32f1c9e339f1d85cb863215
Deven Clingman <dclingy@gmail.com>
2017-08-18 18:04:58 -0600

    added bootstrap gem, generated pages controller for static pages and implemented a nav bar parital

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'home', to: 'pages#home'
+
+  get 'about', to: 'pages#about'
+
+  get 'contact', to: 'pages#contact'
+
+  get 'gallery', to: 'pages#gallery'
+
+  root to: 'pages#home', to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
