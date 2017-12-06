57d85864181d546b27217442bc4732eb471d2b65
Deven Clingman <dclingy@gmail.com>
2017-12-06 15:38:52 -0700

    drastically changed style of homepage, also added email us button on home page also added job model to allow for easier addition of job list

--- app/assets/javascripts/page.js
+++ app/assets/javascripts/page.js
@@ -1,8 +1,5 @@
 $(document).on('turbolinks:load', function() {
 
-  $('#ul-list').hide();
-  $('#what-we-do-btn').click( function () {
-    $('#ul-list').slideToggle(500);
-  });
+  
 
 });
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170913000711) do
+ActiveRecord::Schema.define(version: 20171206215630) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "first_name"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20170913000711) do
     t.string "phone_number"
     t.string "contact_preference"
     t.text "project_description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
